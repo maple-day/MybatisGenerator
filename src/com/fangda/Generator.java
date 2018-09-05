61d89aa14796f11dfb7c964afbd078268a82911c
Yukon <896757055@qq.com>
2018-09-05 17:42:49 +0800

    mybatis逆向生成

---
+++ src/com/fangda/Generator.java
@@ -0,0 +1,25 @@
+package com.fangda;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class Generator {
+
+	public static void main(String[] args) throws Exception {
+		ArrayList<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("genteratorConfig.xml");
+		ConfigurationParser configurationParser = new ConfigurationParser(warnings);
+		Configuration config = configurationParser.parseConfiguration(configFile);
+		DefaultShellCallback shellCallback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+		myBatisGenerator.generate(null);
+		
+	}
+
+}
